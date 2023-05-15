14bbcef2e948da047332bd29862c05667b8b883e
Come Tisserand <cometisserand@rocketmail.com>
2023-05-15 17:25:59 +0200

    feat: bootstrap repo

---
+++ src/main/java/com/example/jenkinscd/JenkinsCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinscd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCdApplication.class, args);
+	}
+
+}
